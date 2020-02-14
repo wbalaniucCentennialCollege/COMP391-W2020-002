fc643080fe3c43890b9c036137c5d148d47c3f5d
Wallace <wallace.balaniuc@gmail.com>
2020-02-14 10:19:07 -0500

    Added laser fire

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Public variables
+    public float speed = 10.0f;
+
+    // Private variables
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = new Vector2(speed, 0.0f);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ public class PlayerController : MonoBehaviour
     // Variables
     public float speed;
     public float minX, minY, maxX, maxY;
+    public GameObject laser;
+    public GameObject laserSpawn;
+    public float fireRate = 0.25f;
 
     private Rigidbody2D rBody;
+    private float timer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +32,21 @@ public class PlayerController : MonoBehaviour
         rBody.position = new Vector2(
             Mathf.Clamp(rBody.position.x, minX, maxX),  // Restricts x position to minX and maxX
             Mathf.Clamp(rBody.position.y, minY, maxY)); // Restricts y position to minY and maxY
+    }
+
+    void Update()
+    {
+        // Check if the "Fire" button is pressed
+        if(Input.GetAxis("Fire1") > 0 && timer > fireRate)
+        {
+            // If yes, spawn the laser
+            GameObject go = GameObject.Instantiate(laser, laserSpawn.transform.position, laserSpawn.transform.rotation);
+            go.transform.Rotate(new Vector3(0, 0, 90));
+
+            // Reset timer
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
     }
 }
