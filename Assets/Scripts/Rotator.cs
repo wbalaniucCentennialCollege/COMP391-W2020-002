fef69c1a6e7944346d22f6d061323e6dda8e668a
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2020-03-06 09:19:55 -0500

    Added explosion sprite

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float tumble = 200;
+
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.angularVelocity = Random.value * tumble;
+    }
+}
