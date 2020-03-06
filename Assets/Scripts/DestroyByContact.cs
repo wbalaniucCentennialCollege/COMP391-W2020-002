35edbae87db4eb4675703bc6265b2b7cf3597b9f
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2020-03-06 12:56:35 -0500

    Explosion

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Detect when another object collides with this object
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Create and position the explosion
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        // Delete the "other" object
+        // Delete this object
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+}
